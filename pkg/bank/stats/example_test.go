e590789ab9d7c18b5ec00c540db0b37ef2ea12d0
Buzurg <buzurgmehr.r@gmail.com>
2020-09-22 11:37:57 +0500

    инициализирован модуль6

--- pkg/bank/stats/example_test.go
+++ pkg/bank/stats/example_test.go
@@ -1,31 +1,55 @@
 package stats
 
 import (
-  "github.com/Buzurgmehr92/bank/pkg/bank/types"
-  "fmt"
+	"fmt"
+
+	"github.com/Buzurgmehr92/bank/pkg/bank/types"
 )
 
+func ExampleAvg() {
+	payments := []types.Payment{
+		{
+			ID:       2,
+			Amount:   55_00,
+			Category: "Cat",
+		},
+		{
+			ID:       1,
+			Amount:   58_00,
+			Category: "Cat",
+		},
+		{
+			ID:       3,
+			Amount:   30_00,
+			Category: "Cat",
+		},
+	}
 
-func ExampleAvg(){
-  payments := []types.Payment{
-    {
-      ID:2,
-      Amount:55_00,
-      Category: "Cat",
-    },
-    {
-      ID:1,
-      Amount:58_00,
-      Category: "Cat",
-    },
-    {
-      ID:3,
-      Amount:30_00,
-      Category: "Cat",
-    },
-  }
+	fmt.Println(Avg(payments))
 
-  fmt.Println(Avg(payments))
+	//Output: 4766
+}
 
-  //Output: 4766
+func ExampleTotalInCategory() {
+	payments := []types.Payment{
+		{
+			ID:       2,
+			Amount:   63_00,
+			Category: "Cafe",
+		},
+		{
+			ID:       1,
+			Amount:   23_00,
+			Category: "Refuelling",
+		},
+		{
+			ID:       3,
+			Amount:   42_00,
+			Category: "Refuelling",
+		},
+	}
+
+	fmt.Println(TotalInCategory(payments, "Refuelling"))
+
+	//Output: 6500
 }
