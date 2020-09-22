6f8525d6325c37938dd46a6b2f43ddf19bdff781
Buzurg <buzurgmehr.r@gmail.com>
2020-09-22 10:37:22 +0500

    инициализирован модуль6

--- pkg/bank/stats/example_test.go
+++ pkg/bank/stats/example_test.go
@@ -10,22 +10,22 @@ func ExampleAvg(){
   payments := []types.Payment{
     {
       ID:2,
-      Amount:53_00,
+      Amount:55_00,
       Category: "Cat",
     },
     {
       ID:1,
-      Amount:51_00,
+      Amount:58_00,
       Category: "Cat",
     },
     {
       ID:3,
-      Amount:52_00,
+      Amount:30_00,
       Category: "Cat",
     },
   }
 
   fmt.Println(Avg(payments))
 
-  //Output: 5200
+  //Output: 4766
 }
